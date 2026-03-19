e8da9bd776fe05b5f44308d34fa9c56ee78b92d5
nalinpa <122588344+nalinpa@users.noreply.github.com>
2026-03-19 16:03:17 +1300

    modular headers fix fir eas ios build v2

--- plugins/withRNMapsModularHeaders.js
+++ plugins/withRNMapsModularHeaders.js
@@ -2,18 +2,30 @@ const { withPodfile } = require("@expo/config-plugins");
 
 module.exports = function withRNMapsModularHeaders(config) {
   return withPodfile(config, (config) => {
-    const podfile = config.modResults.contents;
-    config.modResults.contents = podfile.replace(
-      /post_install do \|installer\|/,
-      `post_install do |installer|
+    const contents = config.modResults.contents;
+
+    const patch = `
   installer.pods_project.targets.each do |target|
     if target.name == 'react-native-maps'
-      target.build_configurations.each do |config|
-        config.build_settings['DEFINES_MODULE'] = 'YES'
+      target.build_configurations.each do |build_config|
+        build_config.build_settings['DEFINES_MODULE'] = 'YES'
       end
     end
-  end`
-    );
+  end`;
+
+    // Try to insert before the end of post_install block
+    if (contents.includes("post_install do |installer|")) {
+      config.modResults.contents = contents.replace(
+        "post_install do |installer|",
+        `post_install do |installer|\n${patch}`
+      );
+    } else {
+      // No post_install block exists, add one before the final 'end'
+      config.modResults.contents =
+        contents +
+        `\npost_install do |installer|\n${patch}\nend\n`;
+    }
+
     return config;
   });
 };
