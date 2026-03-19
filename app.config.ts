824de5d73d06798aa59851d41e160a98d56cd64f
nalinpa <122588344+nalinpa@users.noreply.github.com>
2026-03-19 16:28:48 +1300

    remove withRNMapsModularHeaders plugin from config

--- app.config.ts
+++ app.config.ts
@@ -55,7 +55,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 
   plugins: [
     "expo-router",
-    "./plugins/withRNMapsModularHeaders",
     [
       "@sentry/react-native/expo",
       {
